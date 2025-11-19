779af283bd1c546ab0ea3b1cddecefb07b45c068
Qudix <17361645+Qudix@users.noreply.github.com>
2025-11-18 23:01:22 -0600

    feat: update for AE

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,12 @@ struct Hook_PlayerCharacter
 			a_this->charGenRace = npc->GetFormRace();
 	}
 
-	static inline REL::Relocation<decltype(LoadGame)> _LoadGame;
-
-	static void Install()
-	{
-		REL::Relocation vtbl{ RE::PlayerCharacter::VTABLE[0] };
-		_LoadGame = vtbl.write_vfunc(0x12, LoadGame);
-	}
+	static inline REL::HookVFT _LoadGame{ "LoadGame", RE::PlayerCharacter::VTABLE[0], 0x12, LoadGame };
 };
 
-F4SEPluginLoad(const F4SE::LoadInterface* a_f4se)
+F4SE_PLUGIN_LOAD(const F4SE::LoadInterface* a_f4se)
 {
 	F4SE::Init(a_f4se);
-
-	Hook_PlayerCharacter::Install();
 
 	return true;
 }
--- src/pch.h
+++ src/pch.h
@@ -2,6 +2,3 @@
 
 #include <RE/Fallout.h>
 #include <F4SE/F4SE.h>
-
-namespace logs = F4SE::log;
-using namespace std::literals;
